43625e14c119f568429a22d037a4b55758bb9cca
Pavlo Kyrychenko <81436169+oldstuden@users.noreply.github.com>
2024-04-24 19:47:12 +0200

    add refresh token

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -6,6 +6,8 @@ import { AuthGuard } from "./guards/registration.guards";
 import { LoginUserDto } from "./dto/login-user-dto";
 import { LoginGuard } from "./guards/login.guards";
 import { AuthService } from "./auth.service";
+import { RefreshJwtGuard } from "./guards/refresh-jwt.guard";
+import { RefreshTokenDto } from "./dto/refresh-token.dto";
 
 
 @Controller('auth')
@@ -41,4 +43,36 @@ export class AuthControlller {
 	res.statusCode = HttpStatus.CREATED;
 	return res.send('user created');
 	};
+
+	@UseGuards(RefreshJwtGuard)
+	@Post('refresh')
+	async refreshToken(
+		@Body() refreshTokenDto: RefreshTokenDto,
+		@Res() res: Response,
+	) {const validToken = this.authService.verifyToken(
+      refreshTokenDto.refresh_token,
+    );
+    const user = await this.usersService.findOne(refreshTokenDto.username);
+    const access = await this.authService.generateAccessToken(user);
+
+    if (validToken?.error) {
+      if (validToken?.error === 'jwt expired') {
+        const refresh = await this.authService.generateRefreshToken(
+          user._id as string,
+        );
+
+        res.statusCode = HttpStatus.OK;
+        return res.send({ ...access, ...refresh });
+      } else {
+        res.statusCode = HttpStatus.BAD_REQUEST;
+        return res.send({ error: validToken?.error });
+      }
+    } else {
+      res.statusCode = HttpStatus.OK;
+      return res.send({
+        ...access,
+        refresh_token: refreshTokenDto.refresh_token,
+      });
+    }
+  }	
 }
